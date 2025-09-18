0f53ddac2dfbb9bf1b7a1de7b69115fde968dbe9
HowieYu <howiieyu@gmail.com>
2025-09-19 01:13:37 +0800

    update config

--- init.lua
+++ init.lua
@@ -1,13 +1,16 @@
+vim.opt.relativenumber=true
+vim.opt.number = true
+vim.opt.tabstop = 4
 
 vim.pack.add({
+    "https://github.com/numToStr/Navigator.nvim",
+	"https://github.com/neovim/nvim-lspconfig",
 	"https://github.com/Mofiqul/dracula.nvim",
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
 	"https://github.com/nvim-treesitter/nvim-treesitter-context",
 })
 
-require("dracula").setup({
-	transparent_bg = true,
-})
+require("dracula").setup({ })
 
 vim.cmd[[colorscheme dracula]]
 vim.lsp.config.gopls = {
@@ -18,36 +21,34 @@ vim.lsp.config.gopls = {
 }
 
 vim.lsp.config['lua_ls'] = {
-  -- Command and arguments to start the server.
-  cmd = { 'lua-language-server' },
+	-- Command and arguments to start the server.
+	cmd = { 'lua-language-server' },
 
-  -- Filetypes to automatically attach to.
-  filetypes = { 'lua' },
+	-- Filetypes to automatically attach to.
+	filetypes = { 'lua' },
 
-  -- Sets the "root directory" to the parent directory of the file in the
-  -- current buffer that contains either a ".luarc.json" or a
-  -- ".luarc.jsonc" file. Files that share a root directory will reuse
-  -- the connection to the same LSP server.
-  -- Nested lists indicate equal priority, see |vim.lsp.Config|.
-  root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
-
-  -- Specific settings to send to the server. The schema for this is
-  -- defined by the server. For example the schema for lua-language-server
-  -- can be found here https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
-  settings = {
-    Lua = {
-      runtime = {
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = { 'vim' },
-      },
-    }
-  }
+	-- Sets the "root directory" to the parent directory of the file in the
+	-- current buffer that contains either a ".luarc.json" or a
+	-- ".luarc.jsonc" file. Files that share a root directory will reuse
+	-- the connection to the same LSP server.
+	-- Nested lists indicate equal priority, see |vim.lsp.Config|.
+	root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
+	-- Specific settings to send to the server. The schema for this is
+	-- defined by the server. For example the schema for lua-language-server
+	-- can be found here https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
+	settings = {
+		Lua = {
+			runtime = {
+				version = 'LuaJIT',
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { 'vim' },
+			},
+		}
+	}
 }
 
-vim.lsp.enable('lua_ls')
 vim.api.nvim_create_autocmd('LspAttach', {
 	callback = function(ev)
 		local client = vim.lsp.get_client_by_id(ev.data.client_id)
@@ -55,27 +56,44 @@ vim.api.nvim_create_autocmd('LspAttach', {
 			return
 		end
 		if client:supports_method('textDocument/completion') then
-			vim.opt.completeopt = {'menu', 'menuone', 'noselect', 'fuzzy', 'popup' }
-			vim.lsp.completion.enable(true, client.id, ev.buf, {autotrigger = true})
+		vim.opt.completeopt = { 'menu', 'menuone', 'noselect', 'fuzzy', 'popup' }
+			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
 			vim.keymap.set('i', '<C-Space>', function() vim.lsp.completion.get() end)
-			vim.keymap.set({'i', 's'}, '<Tab>', function()
+			vim.keymap.set({ 'i', 's' }, '<Tab>', function()
 				if vim.fn.pumvisible() then
-					vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true , false, true), 'n', true)
+					vim.api.nvim_feedkeys(
+						vim.api.nvim_replace_termcodes('<C-n>', true, false, true), 'n', true)
 				end
 			end)
-
-			vim.keymap.set({'i', 's'}, '<S-Tab>', function()
+			vim.keymap.set({ 'i', 's' }, '<S-Tab>', function()
 				if vim.fn.pumvisible() then
-					vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-p>', true , false, true), 'n', true)
+					vim.api.nvim_feedkeys(
+						vim.api.nvim_replace_termcodes('<C-p>', true, false, true), 'n', true)
 				end
 			end)
 		end
+
+		vim.keymap.set({'n'}, 'grd', vim.lsp.buf.definition)
+		vim.keymap.set({"n"}, "grD", vim.lsp.buf.declaration)
+		vim.keymap.set({"n", 'v'}, "gr.",vim.lsp.buf.code_action)
 	end
 })
-
 vim.diagnostic.config({
 	virtual_lines = {
 		current_line = true,
 	}
 })
 
+vim.lsp.enable("lua_ls")
+vim.lsp.enable("gopls")
+vim.lsp.enable("clangd")
+
+
+require("Navigator").setup()
+
+vim.keymap.set({'n', 't'}, '<A-h>', '<CMD>NavigatorLeft<CR>')
+vim.keymap.set({'n', 't'}, '<A-l>', '<CMD>NavigatorRight<CR>')
+vim.keymap.set({'n', 't'}, '<A-k>', '<CMD>NavigatorUp<CR>')
+vim.keymap.set({'n', 't'}, '<A-j>', '<CMD>NavigatorDown<CR>')
+vim.keymap.set({'n', 't'}, '<A-p>', '<CMD>NavigatorPrevious<CR>')
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
